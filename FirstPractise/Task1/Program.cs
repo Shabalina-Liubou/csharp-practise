c80af58e9097b8ee1dd5a03811e21147c33b62be
2RoN4eG <2RoN4eG@mail.ru>
2020-03-12 18:20:05 +0300

    Task 1 completed

---
+++ FirstPractise/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short variable = 15;
+            object obj = variable;
+
+            sbyte b_variable = (sbyte)(short)obj;
+
+            Console.WriteLine("Type of b_variable is {0}, its value = {1}", b_variable.GetType().FullName, b_variable);
+
+            Console.ReadLine();
+        }
+    }
+}
