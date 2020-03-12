3dc94fca27ad1fe62904a67ba7707b353d40e84a
2RoN4eG <2RoN4eG@mail.ru>
2020-03-12 18:24:28 +0300

    Task 2 completed

---
+++ FirstPractise/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            // в качестве параметра метод Add() принимает тип object.
+
+            arrayList.Add(45);        //будет, упаковка
+            arrayList.Add(true);      //будет, упаковка
+            arrayList.Add("Hello");   //не будет ни упаковки, ни распаковки
+            arrayList.Add(23.45);     //будет, упаковка
+
+            foreach (object obj in arrayList) // для целей демонстрации, какие типы приходят на упаковку
+            {
+                Console.WriteLine(obj.GetType().FullName);
+            }
+        }
+    }
+}
